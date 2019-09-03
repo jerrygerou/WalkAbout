ceef7d8772ab0b20fb70e8be371d3011ab94e4e7
jerrygerou <jerrysgerou@gmail.com>
2019-09-03 17:39:46 -0500

    Initial Commit

---
+++ WalkAbout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalkAbout
+//
+//  Created by JerryGerou on 9/3/19.
+//  Copyright © 2019 JerryGerou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
